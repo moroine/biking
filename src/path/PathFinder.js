569d232553caaf8abf9fa71c70a2018b3b80aed5
Moroine Bentefrit <moroine.bentefrit@adactive.com>
2018-08-17 23:49:32 +0800

    Optimize pathfinder

--- src/path/PathFinder.js
+++ src/path/PathFinder.js
@@ -5,44 +5,54 @@ class PathFinder {
     this.map = map;
 
     this.cache = new Map();
+
+    this.solutionBestResult = null;
   }
 
-  solveFromStartPoint(row, column) {
+  solveFromStartPoint(row, column, parentElevation = null, parentSize = 0) {
+    const current = this.map.getElevation(row, column);
+
+    if (parentElevation !== null && current >= parentElevation) {
+      return null;
+    }
+
+    // The best size we can achieve on this solution
+    // considering the best overall solution and than elevation cannot be bellow 0
+    const selfSizeLimit = parentSize + current + 1;
+    if (this.solutionBestResult !== null && this.solutionBestResult.size > selfSizeLimit + 1) {
+      return null;
+    }
+
     const cacheResult = this.getCacheResult(row, column);
 
     if (cacheResult !== null) {
       return cacheResult;
     }
 
-    const current = this.map.getElevation(row, column);
-
-    const subCoordinates = [
-      {
-        row: row - 1,
-        column,
-      },
-      {
-        row: row + 1,
-        column,
-      },
-      {
-        row,
-        column: column - 1,
-      },
-      {
-        row,
-        column: column + 1,
-      },
-    ];
-
-    const subResults = subCoordinates.map((coord) => {
-      const elevation = this.map.getElevation(coord.row, coord.column);
-
-      return elevation < current ? this.solveFromStartPoint(coord.row, coord.column) : null;
-    });
-
     const result = new Result(row, column, current);
-    result.addBestSubResult(subResults);
+    let bestSubResult = null;
+
+    const topResult = this.solveFromStartPoint(row - 1, column, current, parentSize + 1);
+    if (Result.isBetterResult(bestSubResult, topResult)) {
+      bestSubResult = topResult;
+    }
+
+    const bottomResult = this.solveFromStartPoint(row + 1, column, current, parentSize + 1);
+    if (Result.isBetterResult(bestSubResult, bottomResult)) {
+      bestSubResult = bottomResult;
+    }
+
+    const leftResult = this.solveFromStartPoint(row, column - 1, current, parentSize + 1);
+    if (Result.isBetterResult(bestSubResult, leftResult)) {
+      bestSubResult = leftResult;
+    }
+
+    const rightResult = this.solveFromStartPoint(row, column + 1, current, parentSize + 1);
+    if (Result.isBetterResult(bestSubResult, rightResult)) {
+      bestSubResult = rightResult;
+    }
+
+    result.addSubResult(bestSubResult);
 
     return this.cacheResult(row, column, result);
   }
@@ -74,19 +84,17 @@ class PathFinder {
   }
 
   solve() {
-    let result = null;
-
     for (let i = 0; i < this.map.rows; i += 1) {
       for (let j = 0; j < this.map.cols; j += 1) {
         const candidate = this.solveFromStartPoint(i, j);
 
-        if (Result.isBetterResult(result, candidate)) {
-          result = candidate;
+        if (Result.isBetterResult(this.solutionBestResult, candidate)) {
+          this.solutionBestResult = candidate;
         }
       }
     }
 
-    return result;
+    return this.solutionBestResult;
   }
 }
 
